ebaa05e9804dcf33be07c223d205534a0ad27429
Rana Husnain <95042433+Rana-Husnain@users.noreply.github.com>
2022-07-03 03:09:08 -0700

    Add files via upload

---
+++ Caculator/index.js
@@ -0,0 +1,102 @@
+
+// function cal (){
+
+
+//     var val1=prompt("enter first value")
+//     var sing=prompt("enter sing value")
+//     var val2=prompt("enter second value")
+    
+//     console.log(val1+sing+val2);
+//     if(sing==="+"){
+//         alert((+val1)+(+val2))
+    
+//     }
+//     else if(sing==="-"){
+//         alert(val1-val2)
+    
+//     }
+//     else if(sing==="*"){
+//         alert(val1*val2)
+    
+//     }
+//     else if(sing==="/"){
+//         alert(val1/val2)
+    
+//     }
+//     else if(sing==="%"){
+//         alert(val1/val2*100)
+    
+//     }
+
+// }
+
+
+
+
+
+
+console.log(val1+sing+val2);
+    if(sing==="+"){
+        alert((+val1)+(+val2))
+    
+    }
+
+
+
+
+
+
+
+
+
+// function empty( ){
+//     var result = document.getElementById("input");
+//     result.value ="" 
+// }
+// function Getnumber(num){
+//     var result = document.getElementById("input");
+//     result.value +=num 
+// }
+
+
+
+
+// function result(){
+
+//     var result = document.getElementById("input");
+
+// result.value = eval(result.value)
+// }
+
+
+
+
+
+
+
+
+function empty(){
+
+var result = document.getElementById("input");
+result.value=""
+
+}
+
+function getNumber(num){
+var result = document.getElementById("input");
+result.value +=num
+
+}
+
+
+function result(){
+    var result = document.getElementById("input");
+    result.value =eval(result.value)
+    
+    }
+
+
+
+
+
+
